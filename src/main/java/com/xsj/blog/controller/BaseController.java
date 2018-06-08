d8a68824af007ba90a280fa673b44ed614bb94a9
xsj <804078333@qq.com>
2018-06-08 11:35:35 +0800

    前后端分离

--- src/main/java/com/xsj/blog/controller/BaseController.java
+++ src/main/java/com/xsj/blog/controller/BaseController.java
@@ -1,7 +1,15 @@
 package com.xsj.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+
+@Controller
 public class BaseController {
 
-
+    public ModelMap getResult(String key,Object value){
+        ModelMap map = new ModelMap();
+        map.put(key,value);
+        return  map;
+    }
 
 }
--- src/main/java/com/xsj/blog/controller/IndexController.java
+++ src/main/java/com/xsj/blog/controller/IndexController.java
@@ -1,13 +1,22 @@
 package com.xsj.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
-    public String index(){
-        return "view/login/index";
+    public String indexPage(){
+        return "index";
     }
+
+    @GetMapping("/{page}")
+    public String jumpPage(@PathVariable(name = "page") String page){
+        return page;
+    }
+
+
 }
