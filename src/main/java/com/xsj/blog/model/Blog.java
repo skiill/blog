40871a82494c17040dbb86584050379452e68e84
xsj <804078333@qq.com>
2018-06-08 15:07:14 +0800

    blog 实体类

---
+++ src/main/java/com/xsj/blog/model/Blog.java
@@ -0,0 +1,68 @@
+package com.xsj.blog.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Blog implements Serializable {
+    private Integer id;
+
+    private String title;
+
+    private Integer aId;
+
+    private Date createtime;
+
+    private Date update;
+
+    private String content;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public Integer getaId() {
+        return aId;
+    }
+
+    public void setaId(Integer aId) {
+        this.aId = aId;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getUpdate() {
+        return update;
+    }
+
+    public void setUpdate(Date update) {
+        this.update = update;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+}
